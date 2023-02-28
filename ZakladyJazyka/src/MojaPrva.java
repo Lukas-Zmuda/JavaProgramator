84078431d666d6802a4049f66ca4783b5303ed4b
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-02-28 15:11:36 +0100

    uloha

--- ZakladyJazyka/src/MojaPrva.java
+++ ZakladyJazyka/src/MojaPrva.java
@@ -1,10 +1,11 @@
-
 public class MojaPrva {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Ahoj Java");
-
+		
+		System.out.println("Lukas");
+		
 	}
 
 }
---
+++ ZakladyJazyka/src/Uloha1.java
@@ -0,0 +1,11 @@
+
+public class Uloha1 {
+
+	//Uloha 1.3 zo strany 31
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
