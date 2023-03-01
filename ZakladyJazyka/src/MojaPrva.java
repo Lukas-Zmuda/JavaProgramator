fc78544c55991cc24a575939ad63800c02b17c17
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-03-01 13:39:17 +0100

    vystup programu
    metody:
    println()
    print()
    printf()

--- ZakladyJazyka/src/MojaPrva.java
+++ ZakladyJazyka/src/MojaPrva.java
@@ -6,6 +6,31 @@ public class MojaPrva {
 		
 		System.out.println("Lukas");
 		
+		System.out.println(10 + 5);
+		
+		System.out.println("10 + 5");
+		
+		System.out.println("Lukas" + "Peter");
+		
+		System.out.println("Lukas" + 5);
+		
+		System.out.println("10" + 5);
+		
+		System.out.print("Janko");
+		System.out.print("Hrasko" + "12");
+		System.out.println("\nFero Mrkva");
+		System.out.println("Ivan\nHrozny");
+		System.out.println("--------------");
+		System.out.println("Meno\tPriezvisko\tVek");
+		System.out.println("Alex\tVelky\t\t45");
+		System.out.println("Jan\tHruska\t\t12");
+		System.out.println("Peter\t\"Bombarder\"\t54");
+		System.out.println("C:\\Windows\\System32\\cmd.exe");
+		
+		System.out.printf("Dnes je %d.%d.%d. Teplota je %f stupnov Celzia",1 ,3 ,2023 , 14.23);
+		
+		
+		
 	}
 
 }
