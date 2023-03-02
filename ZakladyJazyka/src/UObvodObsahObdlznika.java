5975a47e29236d7dd54eebc51deca86d27c36c78
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-03-02 14:52:27 +0100

    Create UObvodObsahObdlznika.java

---
+++ ZakladyJazyka/src/UObvodObsahObdlznika.java
@@ -0,0 +1,17 @@
+
+public class UObvodObsahObdlznika {
+
+	public static void main(String[] args) {
+		
+		int dlzka = 14;
+		int sirka = 37;
+		
+		int obvod = 2 * (dlzka + sirka);
+		int obsah = dlzka * sirka;
+		
+		System.out.println("Obdlznik so stranami " 
+				+ dlzka + " cm, " + sirka + " cm.\nObvod = " + obvod + " cm\nObsah = " + obsah + " cm2");
+
+	}
+
+}
