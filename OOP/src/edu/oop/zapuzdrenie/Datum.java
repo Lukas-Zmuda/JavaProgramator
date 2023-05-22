5c4544505d79e218d221ee222d005703263c2cad
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-05-22 16:01:13 +0200

    uloha Datum

---
+++ OOP/src/edu/oop/zapuzdrenie/Datum.java
@@ -0,0 +1,12 @@
+package edu.oop.zapuzdrenie;
+
+/**
+ * Implementujte triedu podla navrhu z adresy:
+ * https://www3.ntu.edu.sg/home/ehchua/programming/java/j3f_oopexercises.html#show-toc
+ * 
+ * @author lukas
+ *
+ */
+public class Datum {
+
+}
