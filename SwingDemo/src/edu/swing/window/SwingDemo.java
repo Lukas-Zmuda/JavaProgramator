a5de50df507bb3cecb18b9aabd4188bf6245ec4c
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-06-05 16:04:14 +0200

    java swing

---
+++ SwingDemo/src/edu/swing/window/SwingDemo.java
@@ -0,0 +1,51 @@
+package edu.swing.window;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.UIManager;
+
+public class SwingDemo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SwingDemo frame = new SwingDemo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SwingDemo() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SwingDemo.class.getResource("/edu/swing/res/icons8_monkey_36px.png")));
+		setTitle("Moje okno");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 548, 386);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
---
+++ ZakladyJazyka/src/MojeOkno.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class MojeOkno {
+
+	public static void main(String[] args) {
+		
+		JFrame okno = new JFrame();
+		okno.setVisible(true);
+		okno.setTitle("Moje prve okno");
+
+	}
+
+}
